b06189920d4c07044f69f64e71d3f930939b2407
mookar <mookar@Denics-MacBook-Air.local>
2018-09-13 01:05:05 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  let arr = equation.match(/-? ?\d+/gi);
+
+  let a = Number(arr[0]);
+  let b = Number(arr[2].replace(" ",""));
+  let c = Number(arr[3].replace(" ",""));
+  
+  return [Math.round((-b + Math.sqrt((b*b) - 4 * (a * c))) / (2 * a)), 
+          Math.round((-b - Math.sqrt((b*b) - 4 * (a * c))) / (2 * a))].sort((a,b)=>a-b)
 }
